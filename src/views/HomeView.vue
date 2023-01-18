6e1266a926221f155f82f9980a20c03d69ede439
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-01-18 11:51:12 +0100

    :rocket: Initial Commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <main class="home"></main>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Vue, Options } from 'vue-class-component'
 
 @Options({
-  components: {
-    HelloWorld
-  }
 })
 export default class HomeView extends Vue {}
 </script>
+
+<style lang="scss" scoped></style>
